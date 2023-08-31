49f4e153e6cf1d982aaae2c17b01f5e74d23266d
Pvblo <pmonjes@ing.ucsc.cl>
2023-08-31 11:50:33 -0400

    jsclase31-08

---
+++ clase31-08/funciones.js
@@ -0,0 +1,16 @@
+
+
+function colorazul(){
+    document.getElementById("azul").style.backgroundColor="blue";
+}
+
+function colorverde(){
+    document.getElementsByClassName("verde")[0].style.backgroundColor="green";
+
+}
+
+function reset(){
+    document.getElementById("azul").style.backgroundColor="red";
+
+
+}
