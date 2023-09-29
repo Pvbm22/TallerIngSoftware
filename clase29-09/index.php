897fa5c47083adad24710bd33d1f60ceb363b36a
Pvblo <pmonjes@ing.ucsc.cl>
2023-09-29 11:54:48 -0300

    php clase 2 29/09

---
+++ clase29-09/index.php
@@ -0,0 +1,80 @@
+<?php
+
+include("conexion.php");
+$consulta = "SELECT * FROM marcas";
+$respuesta = mysqli_query($conexion, $consulta);
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Marcas</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container p-5">
+        <div class="row">
+            <div class="col">
+                <form action="guardar.php" method="POST">
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1">Nombre</span>
+                        <input type="text" name="nombre" class="form-control" placeholder="Peugeot" required>
+                    </div>
+                    <div class="input-group mb-3">
+                    <span class="input-group-text" id="basic-addon1">Origen</span>
+                        <select class="form-select" name="origen" aria-label="Default select example">
+                        <option selected>Listado</option>
+                        <option value="japon">Japón</option>
+                        <option value="china">China</option>
+                        <option value="francia">Francia</option>
+                    </select>
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1">logo</span>
+                        <input type="text" name="logo" class="form-control" placeholder="../logo-Peugeot.jpg">
+                    </div>
+                    <input type="submit" value="Guardar" class="btn btn-primary">
+                </form>
+            </div>
+
+            
+            <div class="col">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">Origen</th>
+                            <th scope="col">Logo</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        while ($row = mysqli_fetch_assoc($respuesta)) {
+                            echo "<tr>";
+                            echo "<td>" . $row["id"] . "</td>";
+                            echo "<td>" . $row["nombre"] . "</td>";
+                            echo "<td>" . $row["origen"] . "</td>";
+                            echo "<td> <img src=". $row["logo"] ."></td>";
+                            echo "</tr>";
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
